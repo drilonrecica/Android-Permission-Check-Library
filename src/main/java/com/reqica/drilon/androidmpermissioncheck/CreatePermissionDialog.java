5f3b9b153b8f8e26813279612e8ea6a3a361ff7f
Drilon Recica <dreex@adorsys.de>
2016-03-26 14:37:11 +0100

    Fixed some major bugs

--- src/main/java/com/reqica/drilon/androidmpermissioncheck/CreatePermissionDialog.java
+++ src/main/java/com/reqica/drilon/androidmpermissioncheck/CreatePermissionDialog.java
@@ -1,3 +1,28 @@
+/*
+        MIT License
+
+        Copyright (c) 2016 Drilon Reçica
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+
+*/
+
 package com.reqica.drilon.androidmpermissioncheck;
 
 import android.app.Activity;
@@ -10,16 +35,15 @@ import android.support.v7.app.AlertDialog;
 
 public class CreatePermissionDialog {
 
-    static Context context;
-    static Activity activity;
+    Context context;
+    Activity activity;
 
     public CreatePermissionDialog (Context context , Activity activity){
-        CreatePermissionDialog.context = context;
-        CreatePermissionDialog.activity = activity;
+        this.context = context;
+        this.activity = activity;
     }
 
-    public static void checkMPermission (final String permission , String dialogMessage) {
-
+    public void checkMPermission (final String permission , String dialogMessage) {
 
         int hasSpecificPermission = 0;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
@@ -51,7 +75,7 @@ public class CreatePermissionDialog {
 
 
 
-    private static void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
         new AlertDialog.Builder(context)
                 .setCancelable(false)
                 .setMessage(message)
--- src/main/java/com/reqica/drilon/androidmpermissioncheck/PERMISSIONS.java
+++ src/main/java/com/reqica/drilon/androidmpermissioncheck/PERMISSIONS.java
@@ -1,37 +1,64 @@
+/*
+        MIT License
+
+        Copyright (c) 2016 Drilon Reçica
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+
+*/
+
 package com.reqica.drilon.androidmpermissioncheck;
+
+import android.Manifest;
 
 public class PERMISSIONS {
 
     protected static final int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
-    public static final String READ_CALENDAR = "Manifest.permission.READ_CALENDAR";
-    public static final String WRITE_CALENDAR = "Manifest.permission.WRITE_CALENDAR";
-    public static final String CAMERA = "Manifest.permission.CAMERA";
-    public static final String READ_CONTACTS = "Manifest.permission.READ_CONTACTS";
+    public static final String READ_CALENDAR = Manifest.permission.READ_CALENDAR;
+    public static final String WRITE_CALENDAR = Manifest.permission.WRITE_CALENDAR;
+    public static final String CAMERA = Manifest.permission.CAMERA;
+    public static final String READ_CONTACTS = Manifest.permission.READ_CONTACTS;
 
-    public static final String WRITE_CONTACTS = "Manifest.permission.WRITE_CONTACTS";
-    public static final String GET_ACCOUNTS = "Manifest.permission.GET_ACCOUNTS";
-    public static final String ACCESS_FINE_LOCATION = "Manifest.permission.ACCESS_FINE_LOCATION";
-    public static final String ACCESS_COARSE_LOCATION = "Manifest.permission.ACCESS_COARSE_LOCATION";
+    public static final String WRITE_CONTACTS = Manifest.permission.WRITE_CONTACTS;
+    public static final String GET_ACCOUNTS = Manifest.permission.GET_ACCOUNTS;
+    public static final String ACCESS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    public static final String ACCESS_COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
 
-    public static final String RECORD_AUDIO = "Manifest.permission.RECORD_AUDIO";
-    public static final String READ_PHONE_STATE = "Manifest.permission.READ_PHONE_STATE";
-    public static final String CALL_PHONE = "Manifest.permission.CALL_PHONE";
-    public static final String READ_CALL_LOG = "Manifest.permission.READ_CALL_LOG";
+    public static final String RECORD_AUDIO = Manifest.permission.RECORD_AUDIO;
+    public static final String READ_PHONE_STATE = Manifest.permission.READ_PHONE_STATE;
+    public static final String CALL_PHONE = Manifest.permission.CALL_PHONE;
+    public static final String READ_CALL_LOG = Manifest.permission.READ_CALL_LOG;
 
-    public static final String WRITE_CALL_LOG = "Manifest.permission.WRITE_CALL_LOG";
-    public static final String ADD_VOICEMAINL = "Manifest.permission.ADD_VOICEMAIL";
-    public static final String USE_SIP = "Manifest.permission.USE_SIP";
-    public static final String PROCESS_OUTGOING_CALLS = "Manifest.permission.PROCESS_OUTGOING_CALLS";
+    public static final String WRITE_CALL_LOG = Manifest.permission.WRITE_CALL_LOG;
+    public static final String ADD_VOICEMAINL = Manifest.permission.ADD_VOICEMAIL;
+    public static final String USE_SIP = Manifest.permission.USE_SIP;
+    public static final String PROCESS_OUTGOING_CALLS = Manifest.permission.PROCESS_OUTGOING_CALLS;
 
-    public static final String BODY_SENSORS = "Manifest.permission.BODY_SENSORS";
-    public static final String SEND_SMS = "Manifest.permission.SEND_SMS";
-    public static final String RECEIVE_SMS = "Manifest.permission.RECEIVE_SMS";
-    public static final String READ_SMS = "Manifest.permission.READ_SMS";
+    public static final String BODY_SENSORS = Manifest.permission.BODY_SENSORS;
+    public static final String SEND_SMS = Manifest.permission.SEND_SMS;
+    public static final String RECEIVE_SMS = Manifest.permission.RECEIVE_SMS;
+    public static final String READ_SMS = Manifest.permission.READ_SMS;
 
-    public static final String RECEIVE_WAP_PUSH = "Manifest.permission.RECEIVE_WAP_PUSH";
-    public static final String RECEIVE_MMS = "Manifest.permission.RECEIVE_MMS";
-    public static final String WRITE_EXTERNAL_STORAGE = "Manifest.permission.WRITE_EXTERNAL_STORAGE";
-    public static final String READ_EXTERNAL_STORAGE = "Manifest.permission.READ_EXTERNAL_STORAGE";
+    public static final String RECEIVE_WAP_PUSH = Manifest.permission.RECEIVE_WAP_PUSH;
+    public static final String RECEIVE_MMS = Manifest.permission.RECEIVE_MMS;
+    public static final String WRITE_EXTERNAL_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+    public static final String READ_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE;
 
 }
