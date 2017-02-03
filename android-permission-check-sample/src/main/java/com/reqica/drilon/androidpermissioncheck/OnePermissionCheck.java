b187fe2c4d6382ac1ba482a3e66491d75f1c3241
Drilon Recica <dre@adorsys.de>
2017-02-03 17:02:00 +0100

    Different Use-cases for permission checks

--- android-permission-check-sample/src/main/java/com/reqica/drilon/androidpermissioncheck/OnePermissionCheck.java
+++ android-permission-check-sample/src/main/java/com/reqica/drilon/androidpermissioncheck/OnePermissionCheck.java
@@ -2,6 +2,8 @@ package com.reqica.drilon.androidpermissioncheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.reqica.drilon.androidpermissionchecklibrary.CheckPermission;
 import com.reqica.drilon.androidpermissionchecklibrary.Permission;
@@ -13,8 +15,31 @@ public class OnePermissionCheck extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_one_permission_check);
 
-		CheckPermission checkPermission = new CheckPermission(this);
+		final CheckPermission checkPermission = new CheckPermission(this);
 
-		checkPermission.checkOne(Permission.CAMERA, "Test");
+		Button checkWithMessageBtn = (Button) findViewById(R.id.with_message_btn);
+		Button checkWithoutMessageBtn = (Button) findViewById(R.id.without_message_btn);
+		Button revokePermissionBtn = (Button) findViewById(R.id.revoke_permission_btn);
+
+		checkWithMessageBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				checkPermission.checkOne(Permission.CAMERA, "TestMessage");
+			}
+		});
+
+		checkWithoutMessageBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				checkPermission.checkOne(Permission.CAMERA, null);
+			}
+		});
+
+		revokePermissionBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				checkPermission.revokePermission(Permission.CAMERA);
+			}
+		});
 	}
 }
