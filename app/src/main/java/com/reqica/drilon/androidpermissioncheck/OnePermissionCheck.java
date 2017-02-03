b8bf908c26106993550c09ebd12f3ccffeef9c61
Drilon Recica <dre@adorsys.de>
2017-02-03 15:00:39 +0100

    Created empty test activities

---
+++ app/src/main/java/com/reqica/drilon/androidpermissioncheck/OnePermissionCheck.java
@@ -0,0 +1,13 @@
+package com.reqica.drilon.androidpermissioncheck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnePermissionCheck extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_one_permission_check);
+	}
+}
