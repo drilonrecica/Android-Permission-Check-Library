3c248ca5b2882ddca5b282cb3d4126514979ae6d
Drilon Recica <dre@adorsys.de>
2017-02-03 15:04:33 +0100

    Linked test activities with MainActivity

--- app/src/main/java/com/reqica/drilon/androidpermissioncheck/MainActivity.java
+++ app/src/main/java/com/reqica/drilon/androidpermissioncheck/MainActivity.java
@@ -1,7 +1,10 @@
 package com.reqica.drilon.androidpermissioncheck;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button checkOnePermissionBtn =
+				(Button) findViewById(R.id.check_one_permission_btn);
+		Button checkMultiplePermissionsBtn =
+				(Button) findViewById(R.id.check_multiple_permissions_btn);
+
+		checkOnePermissionBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(MainActivity.this, OnePermissionCheck.class));
+			}
+		});
+
+		checkMultiplePermissionsBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(MainActivity.this, MultiplePermissionCheck.class));
+			}
+		});
 	}
 }
