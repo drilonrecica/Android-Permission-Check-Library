6e89745cbe7ecb1ae8b7a0dc9c89be81257e4079
Drilon Recica <dre@adorsys.de>
2017-02-09 21:26:26 +0100

    Testing new tagging implementation

--- set_tags.sh
+++ set_tags.sh
@@ -1,10 +1,11 @@
 BRANCH="master"
+RELEASE_VERSION=./gradlew -q pV
 
-## Are we on the right branch?
+# Are we on the right branch?
 #if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
-#
-#  # Is this not a Pull Request?
-#  if [ "$TRAVIS_PULL_REQUEST" = false ]; then
+
+  # Is this not a Pull Request?
+  if [ "$TRAVIS_PULL_REQUEST" = false ]; then
 
     # Is this not a build which was triggered by setting a new tag?
     if [ -z "$TRAVIS_TAG" ]; then
@@ -14,11 +15,12 @@ BRANCH="master"
       git config --global user.name "Travis"
 
       # Add tag and push to master.
-      git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
+#      git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
+      git tag -a v"$RELEASE_VERSION" -m "Travis build $RELEASE_VERSION pushed a tag."
       git push origin --tags
       git fetch origin
 
       echo -e "Done magic with tags.\n"
-#  fi
-#  fi
-fi
+  fi
+  fi
+#fi
